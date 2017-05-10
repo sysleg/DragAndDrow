263f787af8510c2578df1dce1893df23f5ca97e7
sysleg <zlou.sysleg@gmail.com>
2017-05-10 20:58:45 +0300

    Реализован класс потом View, с помощью которого можно рисовать на экране красные прямоуголники

--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -1,13 +1,14 @@
 package com.bignerdranch.android.draganddraw;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DragAndDrawActivity extends AppCompatActivity {
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drag_and_draw);
+    protected Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
     }
 }
